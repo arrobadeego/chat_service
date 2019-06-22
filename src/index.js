c31246a92c1a31ef3729a10943dadad23a3935b2
Diego Moreira <diegomptcc@hotmail.com>
2019-06-22 17:37:50 -0300

    configure and fix eslint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://admin:12345@cluster0-vybfh.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.use(require('./routes'));
