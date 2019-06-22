e2d32b57c2b67a3f05f27dddde7371fdac170e3d
Diego Moreira <diegomptcc@hotmail.com>
2019-06-22 16:52:10 -0300

    configure mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:12345@cluster0-vybfh.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 app.listen(3333);
