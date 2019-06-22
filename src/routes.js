682fa24916e27acc0bd32c9f5978089171ad0b08
Diego Moreira <diegomptcc@hotmail.com>
2019-06-22 18:37:53 -0300

    create user store route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const UserController = require('./users/UserController');
 
 const routes = new express.Router();
+
+routes.post('/users', UserController.store);
 
 module.exports = routes;
