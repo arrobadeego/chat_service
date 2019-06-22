8049a1414fa8cbc18f8a24d26df083ce956a881e
Diego Moreira <diegomptcc@hotmail.com>
2019-06-22 18:55:59 -0300

    include socket io

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const cors = require('cors');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb+srv://admin:12345@cluster0-vybfh.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
 });
 
 app.use(cors());
--- src/users/UserController.js
+++ src/users/UserController.js
@@ -3,7 +3,7 @@ const User = require('./User');
 module.exports = {
 
   async store(req, res) {
-      console.log(req.body);
+    console.log(req.body);
     const {
       name, email, password, status,
     } = req.body;
