48d2264cb7e79f9e7feaaae1e4c1bc9417df63b7
Diego Moreira <diegomptcc@hotmail.com>
2019-06-23 14:16:30 -0300

    create contacts

---
+++ src/contacts/ContactController.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../users/User');
+const authConfig = require('../config/auth.json');
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
+module.exports = {
+
+  async store(req, res) {
+    jwt.verify(req.headers.token, authConfig.secret, async (err, decoded) => {
+      const user = await User.findById(decoded.id);
+      if (err) return res.json(err);
+      user.contacts.push(req.body.id);
+      return res.json({ user });
+    });
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const UserController = require('./users/UserController');
+const ContactController = require('./contacts/ContactController');
 
 const routes = new express.Router();
 
 routes.post('/login', UserController.authenticate);
 routes.post('/registration', UserController.store);
+routes.post('/contacts', ContactController.store);
 
 module.exports = routes;
