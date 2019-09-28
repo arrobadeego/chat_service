6f89900c7de6586d636527d61fb7fb2847eb2208
Diego Moreira <diegomptcc@hotmail.com>
2019-09-28 19:58:41 -0300

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-// const WebSocket = require('ws');
-// const MasterSocket = require('./MasterSocket');
 const routes = require('./routes/routes');
 
 require('./database');
@@ -12,11 +10,7 @@ class App {
     this.app = express();
 
     this.server = require('http').Server(this.app);
-
-    // this.myWebsocket = new WebSocket.Server({ server: this.server });
     this.io = require('socket.io')(this.server);
-
-    // this.socket = new MasterSocket(this.myWebsocket);
 
     this.middlewares();
 
